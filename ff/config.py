99a96178396dc383750f4e817035e40872e5628d
hyperdude <hyperdude@clout.net>
2018-10-28 23:32:01 -0700

    add config file

---
+++ ff/config.py
@@ -0,0 +1,10 @@
+from modules.naughty import Naughty
+from modules.overflow import OverflowFuzzer
+
+# MODULES
+MODS = {
+    "overflow": OverflowFuzzer, 
+    "naughty": Naughty
+    }
+
+BORDER = "-"*40
